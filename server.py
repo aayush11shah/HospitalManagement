00ee5e3ac53f1c1639597404fbbd21afa33af56f
Arjav Garg <arjavgarg@gmail.com>
2021-04-04 20:18:09 +0530

    serving html now

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+import os
+app = flask.Flask(__name__, template_folder='HTML')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def homepage():
+    return flask.render_template("website_home.html")
+    
+@app.route('/<user_type>/<page_type>')
+def page(user_type, page_type):
+    return flask.render_template(user_type+"/"+user_type+"_"+page_type+".html")
     
 if __name__ == '__main__':
     app.run()
