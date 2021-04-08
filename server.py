9af4c1b85ff21ecb502c34c7bf385e92e0861ad8
Arjav Garg <arjavgarg@gmail.com>
2021-04-08 22:13:39 +0530

    serving html files

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 import flask
 import os
-app = flask.Flask(__name__, template_folder='HTML')
+app = flask.Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return flask.render_template("website_home.html")
+    return flask.render_template("home.html")
     
 @app.route('/<user_type>/<page_type>')
 def page(user_type, page_type):
-    return flask.render_template(user_type+"/"+user_type+"_"+page_type+".html")
+    return flask.render_template(page_type)
     
 if __name__ == '__main__':
     app.run()
