bc4101bad444afa5637eb53d111b21e4f53c5ed7
Arjav Garg <arjavgarg@gmail.com>
2021-04-10 12:48:54 +0530

    separated post request of patient register form

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template("home.html")
-    
+
+
 @app.route('/patient_register', methods=['POST'])
 def register_patient():
     form = request.form
@@ -16,8 +17,6 @@ def register_patient():
 @app.route('/<page_type>', methods=['GET'])
 def page(page_type):
     return render_template(page_type)
-    
-
     
 if __name__ == '__main__':
     app.run()
